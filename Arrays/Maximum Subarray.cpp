c33266eff8b70d61aee37e9151bfc64a7ab7c3de
Anvay Yamijala <71481215+anvayyamijala@users.noreply.github.com>
2022-06-10 14:19:36 +0530

    Update Maximum Subarray.cpp

--- Arrays/Maximum Subarray.cpp
+++ Arrays/Maximum Subarray.cpp
@@ -10,11 +10,12 @@ Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6. */
 
+//Using Kadane's Algorithm - Important algorithm
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        //Using Kadane's Algorithm
+    int maxSubArray(vector<int>& nums) 
+    {
         int res = nums[0];
         int maxEnding = nums[0];
         

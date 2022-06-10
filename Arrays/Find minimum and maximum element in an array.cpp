a09b049eef2d86936ff48e8186779d56f0281517
Anvay Yamijala <71481215+anvayyamijala@users.noreply.github.com>
2022-06-10 14:18:34 +0530

    Update Find minimum and maximum element in an array.cpp

--- Arrays/Find minimum and maximum element in an array.cpp
+++ Arrays/Find minimum and maximum element in an array.cpp
@@ -1,3 +1,12 @@
+/* Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
+
+Example:
+        Input:
+                N = 6
+                A[] = {3, 2, 1, 56, 10000, 167}
+        Output:
+                min = 1, max =  10000 */
+
 #include <bits/stdc++.h>
 using namespace std;
 
